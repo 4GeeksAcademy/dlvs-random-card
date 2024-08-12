244a0b388394690036c72e1fc93defc59aac8621
Danny Valdivia <dluis.valdivia@gmail.com>
2024-08-12 16:13:26 +0000

    feat: ranndom card

--- src/app.js
+++ src/app.js
@@ -4,9 +4,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-/* window.onload = function() {
-  document.querySelector("#card").innerHTML = cardGenerator;
-♦ ♥ ♠ ♣
-  function cardGenerator() {}
+window.onload = function() {
+  const cardNumber = [
+    "Ace",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  const randomCardNumber = Math.floor(Math.random() * cardNumber.length);
+  const icons = ["♦", "♥", "♠", "♣"];
+  const randomIcon = icons[Math.floor(Math.random() * icons.length)];
+
+  document.querySelector("#top-left-icon").textContent = randomIcon;
+  document.querySelector("#center-number").textContent = randomCardNumber;
+  document.querySelector("#bottom-right-icon").textContent = randomIcon;
 };
- */
