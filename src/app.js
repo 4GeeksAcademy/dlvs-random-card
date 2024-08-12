4059816ce3ef175c5c73312747c883046318f168
Danny Valdivia <dluis.valdivia@gmail.com>
2024-08-12 18:15:58 +0000

    final version

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   const cardNumber = [
-    "Ace",
+    "A",
     "2",
     "3",
     "4",
@@ -20,11 +20,21 @@ window.onload = function() {
     "Q",
     "K"
   ];
-  const randomCardNumber = Math.floor(Math.random() * cardNumber.length);
+  const randomCardNumber =
+    cardNumber[Math.floor(Math.random() * cardNumber.length)];
   const icons = ["♦", "♥", "♠", "♣"];
   const randomIcon = icons[Math.floor(Math.random() * icons.length)];
 
   document.querySelector("#top-left-icon").textContent = randomIcon;
   document.querySelector("#center-number").textContent = randomCardNumber;
   document.querySelector("#bottom-right-icon").textContent = randomIcon;
+
+  if (randomIcon === "♦" || randomIcon === "♥") {
+    document.querySelector("#top-left-icon").style.color = "red";
+    document.querySelector("#bottom-right-icon").style.color = "red";
+  }
 };
+
+setInterval(function() {
+  window.location.reload();
+}, 5000);
